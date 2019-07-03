f9c717d7223c4d13303d67b9412f9a2b0de2ad46
Fedorchenko Anton <antony.corwin@gmail.com>
2019-07-03 23:13:39 +0300

    build

---
+++ build/precache-manifest.23c18290502f4a5abeeb305b492039f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4af8cb407419e6f27eadc8e38684cb4",
+    "url": "/goit-react-hw-03-image-finder/build/index.html"
+  },
+  {
+    "revision": "32ac7557190e6879c254",
+    "url": "/goit-react-hw-03-image-finder/build/static/css/2.9d8ef868.chunk.css"
+  },
+  {
+    "revision": "0f9fac49e0c72b9975f5",
+    "url": "/goit-react-hw-03-image-finder/build/static/css/main.b6f812ca.chunk.css"
+  },
+  {
+    "revision": "32ac7557190e6879c254",
+    "url": "/goit-react-hw-03-image-finder/build/static/js/2.96f38251.chunk.js"
+  },
+  {
+    "revision": "0f9fac49e0c72b9975f5",
+    "url": "/goit-react-hw-03-image-finder/build/static/js/main.9363eb6b.chunk.js"
+  },
+  {
+    "revision": "dc2795c01ac524320e26",
+    "url": "/goit-react-hw-03-image-finder/build/static/js/runtime~main.2b83f4db.js"
+  }
+]);
